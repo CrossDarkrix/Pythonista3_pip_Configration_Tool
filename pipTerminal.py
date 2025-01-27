d1da59da06f9f90c60213641fed9fe46e90d45bd
CrossDarkrix <crosseddarkrix@gmail.com>
2025-01-27 19:42:48 +0900

    Update pipTerminal.py

--- pipTerminal.py
+++ pipTerminal.py
@@ -818,7 +818,7 @@ class pipTerminal(object):
                                         print(env_data)
                                 else:
                                     if Args[1][0] == '"' and Args[1][-1] == '"':
-                                        print(' '.join(re.findall('"(.+)"', Args[1]))
+                                        print(' '.join(re.findall('"(.+)"', Args[1])))
                             except:
                                 print(Args[1])
                             
